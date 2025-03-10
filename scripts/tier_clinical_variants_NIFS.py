eec699eb950261da55189ea02da3698661879913
Eren Shin <eshin0525@gmail.com>
2025-03-10 16:36:31 -0400

    updated tiering script

--- scripts/tier_clinical_variants_NIFS.py
+++ scripts/tier_clinical_variants_NIFS.py
@@ -21,7 +21,6 @@ GQ_threshold = int(args.GQ_threshold)
 inheritance_type = args.type
 
 clnrevstat_one_star_plus = ["criteria_provided,_multiple_submitters,_no_conflicts", "criteria_provided,_single_submitter", "practice_guideline", "reviewed_by_expert_panel"]
-PTVs = ['frameshift_variant', 'stop_gained', 'splice_donor_variant', 'splice_acceptor_variant', 'transcript_ablation']
 
 df = pd.read_csv(uri, sep='\t')
 # Strip quotes etc. from every column
@@ -39,6 +38,7 @@ df.loc[passes_filters, 'Tier'] = 4
 # Tier 3: Include VUS or Conflicting in ClinVar
 vus_or_conflicting_in_clinvar = (df['info.CLNSIG'].str.contains('Uncertain') | df['info.CLNSIG'].str.contains('Conflicting'))
 df.loc[passes_filters & vus_or_conflicting_in_clinvar, 'Tier'] = 3
+
 # CRITERIA FOR BOTH TIER 1 AND TIER 2
 is_clinvar_P_LP = ((df['info.CLNSIG'].astype(str).str.contains('athogenic')) 
                 & (~df['info.CLNSIG'].astype(str).str.contains('Conflicting')))
@@ -47,58 +47,59 @@ is_clinvar_P_LP_one_star_plus = is_clinvar_P_LP & is_clnrevstat_one_star_plus
 not_in_clinvar = df['info.CLNSIG'].isna()
 is_clinvar_P_LP_one_star_plus_or_not_in_clinvar = is_clinvar_P_LP_one_star_plus | not_in_clinvar
 
-in_any_gene_list = ~df['vep.transcript_consequences.gene_list'].isna()
-
-not_in_segdup_str_simplerep = (~df[['info.SEGDUP', 'info.STR', 'info.SIMPLEREP']].any(axis=1))
+not_in_segdup_str_simplerep = (~df[['info.SEGDUP', 'info.STR', 'info.SIMPLEREP']].any(axis=1))  # Tier 1
+not_in_segdup = (~df['info.SEGDUP'])  # Tier 2
 
 ncount_over_proband_DP = df['info.NCount'] / df['proband_entry.DP']
 passes_ncount_over_proband_DP = (ncount_over_proband_DP < ncount_over_proband_DP_threshold)
 
 passes_ECNT = (df['info.ECNT'] < ECNT_threshold)
 
-high_or_moderate_impact = df['vep.transcript_consequences.IMPACT'].isin(['HIGH','MODERATE'])
+high_impact = (df['vep.transcript_consequences.IMPACT']=='HIGH')  # Tier 1
+high_or_moderate_impact = df['vep.transcript_consequences.IMPACT'].isin(['HIGH','MODERATE'])  # Tier 2
 
-not_inframe_indel = (~df['vep.transcript_consequences.Consequence'].str.contains('inframe'))
 # GT CRITERIA
 if inheritance_type=='recessive':
-    # Treat XLR like AD
+    # Treat XLR like AD (proband has alt allele)
     xlr_proband_GT = ((df.locus.str.contains('X')) & 
                       (df['proband_entry.GT'].str.contains('1')))
-    # Only HomVar for Tier 1 (except XLR)
+    # Tier 1: Only HomVar (except XLR)
     tier_1_proband_GT = (df['proband_entry.GT'].isin(['1/1','1|1'])) | (xlr_proband_GT)
-    # Allow Het for Tier 2 if mother has variant
-    tier_2_proband_GT = ((tier_1_proband_GT) | 
-                         ((df['proband_entry.GT'].isin(['0/1','0|1'])) & (df['mother_entry.GT']!='0/0')))
+    # Tier 2: Exclude C0 for AR, except for comphets
+    tier_2_proband_GT = (df['variant_category']=='comphet') | (df['info.CA_from_GT']!=0)
 
 if inheritance_type=='dominant':
+    # Tier 1: Proband has alt allele
     tier_1_proband_GT = (df['proband_entry.GT'].str.contains('1'))
-    tier_2_proband_GT = tier_1_proband_GT
+    # Tier 2: No proband alt allele filter
+    tier_2_proband_GT = (df['proband_entry.GT']!='')
 
-passes_tier_1_and_2 = (is_clinvar_P_LP_one_star_plus_or_not_in_clinvar &
-                        in_any_gene_list &
-                        not_in_segdup_str_simplerep &
-                        passes_ncount_over_proband_DP &
+passes_tier_1_and_2 = (passes_ncount_over_proband_DP &
                         passes_ECNT &
-                        high_or_moderate_impact &
-                        not_inframe_indel &
                         passes_filters)
 
-# Tier 2: ClinVar P/LP 1*+, in gene list, not in SEGDUP/STR/SIMPLEREP, etc.
-df.loc[passes_tier_1_and_2 & tier_2_proband_GT, 'Tier'] = 2
-# CRITERIA FOR JUST TIER 1
-passes_GQ = (df['proband_entry.GQ']>GQ_threshold)
-is_PTV = df['vep.transcript_consequences.Consequence'].apply(lambda csq_str:
-                                                             any([csq in csq_str for csq in PTVs]))
+# Tier 2: ClinVar P/LP 1*+ OR HIGH/MODERATE IMPACT, not in SEGDUP, etc.
+df.loc[(is_clinvar_P_LP_one_star_plus_or_not_in_clinvar | high_or_moderate_impact) &
+        passes_tier_1_and_2 & 
+        not_in_segdup &
+        tier_2_proband_GT, 'Tier'] = 2
 
-# Tier 1: ClinVar P/LP 1*+, in gene list, not in SEGDUP/STR/SIMPLEREP, etc. + GQ filter + PTVs
-df.loc[passes_tier_1_and_2 & 
-       passes_GQ &
-       is_PTV &
-       tier_1_proband_GT, 'Tier'] = 1
+# Tier 1: ClinVar P/LP 1*+ OR HIGH IMPACT, not in SEGDUP/STR/SIMPLEREP, etc.
+df.loc[(is_clinvar_P_LP_one_star_plus_or_not_in_clinvar | high_impact) &
+        passes_tier_1_and_2 & 
+        not_in_segdup_str_simplerep &
+        tier_1_proband_GT, 'Tier'] = 1
 
 # Add flags for LQ and VUS
 df['Tier'] = df['Tier'].astype(str)
-df.loc[~passes_GQ, 'Tier'] = df.loc[~passes_GQ, 'Tier'] + ';LQ'
+df.loc[(~passes_ECNT | ~passes_ncount_over_proband_DP), 'Tier'] = df.loc[(~passes_ECNT | ~passes_ncount_over_proband_DP), 'Tier'] + ';LQ'
 df.loc[vus_or_conflicting_in_clinvar, 'Tier'] = df.loc[vus_or_conflicting_in_clinvar, 'Tier'] + ';VUS'
+
+# maternal_carrier column
+is_maternal_variant = df['mother_entry.GT'].str.contains('1')
+df['maternal_carrier'] = ((is_clinvar_P_LP_one_star_plus_or_not_in_clinvar | high_or_moderate_impact) &
+       passes_ECNT &
+       passes_ncount_over_proband_DP &
+       is_maternal_variant)
 
 df.to_csv(output_uri, sep='\t', index=False)

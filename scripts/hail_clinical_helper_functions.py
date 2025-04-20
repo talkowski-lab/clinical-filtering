220f27a2b0a1095c604474e9165554ad93bcd70c
Eren Shin <eshin0525@gmail.com>
2025-04-20 08:40:29 -0400

    Added sort_final_merged_output_by_tiers to hail_clinical_helper_functions.py

--- scripts/hail_clinical_helper_functions.py
+++ scripts/hail_clinical_helper_functions.py
@@ -8,6 +8,8 @@
 ###
 from typing import Tuple
 
+import pandas as pd
+import numpy as np
 import hail as hl
 import hail.expr.aggregators as agg
 from hail.expr import expr_call, expr_float64
@@ -416,3 +418,54 @@ def get_transmission(phased_tm):
                                 hl.or_missing(phased_tm.proband_entry.PBT_GT==hl.parse_call('1|1'), 'inherited_from_both'))))
     )
     return phased_tm
+
+def sort_final_merged_output_by_tiers(merged_df):
+    # Sort by tier (lower = higher priority)
+    def get_len_of_top_numeric_tier(row):
+        top_numeric_tier = row.top_numeric_tier
+        numeric_tiers = row.numeric_tiers_list
+        top_tier = row.tiers_list[numeric_tiers.index(top_numeric_tier)]
+        return len(top_tier)
+
+    merged_df['tiers_list'] = merged_df.Tier.str.split(',')  # with * and flags
+    merged_df['numeric_tiers_list'] = merged_df.tiers_list.apply(lambda lst: [int(x[0])  if x!='' else 6 for x in lst])  # Assign missing tier to tier 6 for sorting
+    merged_df['top_numeric_tier'] = merged_df.numeric_tiers_list.apply(min)
+    merged_df['top_tier_len'] = merged_df.apply(get_len_of_top_numeric_tier, axis=1)  # Longer = worse tier!
+    merged_df['all_tiers_len'] = merged_df['Tier'].apply(len)  # Longer = worse (more "bad" tiers included)
+    # Get best tier for comphets
+    merged_df['top_numeric_tier_comphet'] = merged_df['top_numeric_tier']
+    merged_df.loc[merged_df.variant_category.str.contains('comphet'), 'top_numeric_tier_comphet'] = merged_df.comphet_ID.map(
+                    merged_df[merged_df.variant_category.str.contains('comphet')].groupby('comphet_ID')['top_numeric_tier'].min())
+
+    # Pull flags from Tier column and move to filters column
+    def get_flags_from_tier_list(tier_list):
+        flags = [';'.join(x.split(';')[1:]) for x in tier_list]
+        flags = [x for x in flags if x!='']
+        unique_flags = list(set(flags))
+        if len(unique_flags)==0:
+            return np.nan
+        return unique_flags[0]
+
+    def update_filters_with_flags(row):
+        if pd.isna(row.tier_filter):  # no new flags to add
+            return row.filters
+        if pd.isna(row.filters):  # no existing filters
+            return row.tier_filter
+        return row.filters + ',' + row.tier_filter
+
+    merged_df['tier_filter'] = merged_df.Tier.str.split(',').apply(get_flags_from_tier_list)
+    merged_df['filters'] = merged_df.apply(update_filters_with_flags, axis=1)
+
+    # Update Tier column to just have Tier (including *, but NO flags)
+    merged_df['Tier'] = merged_df.Tier.str.split(',').apply(lambda lst: [x.split(';')[0] for x in lst]).apply(','.join)
+
+    tmp_tier_cols = ['tiers_list','numeric_tiers_list','top_numeric_tier_comphet','top_numeric_tier','top_tier_len','all_tiers_len','tier_filter']
+    # Sort by sample ID first!
+    merged_df = merged_df.sort_values(['id','top_numeric_tier_comphet','comphet_ID','top_numeric_tier','top_tier_len','all_tiers_len']).drop(tmp_tier_cols, axis=1)
+
+    # Strip out leading commas
+    for col in merged_df.columns:
+        if merged_df[col].dtype=='object':
+            merged_df[col] = merged_df[col].replace({np.nan: ''}).astype(str).str.lstrip(',').replace({'': np.nan})
+    
+    return merged_df
